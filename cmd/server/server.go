8a3cd9c18e243f24a09aefb7b40ef508ac59e2ef
AntonPodash <apodash01@gmail.com>
2021-06-04 00:38:19 +0300

    refactor(api)

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,56 +1,149 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
+	"errors"
 	"flag"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
-	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/podash/load_balancer/httptools"
 	"github.com/podash/load_balancer/signal"
 )
 
-var port = flag.Int("port", 8080, "server port")
+var (
+	port       = flag.Int("port", 8090, "load balancer port")
+	timeoutSec = flag.Int("timeout-sec", 3, "request timeout time in seconds")
+	https      = flag.Bool("https", false, "whether backends support HTTPs")
 
-const confResponseDelaySec = "CONF_RESPONSE_DELAY_SEC"
-const confHealthFailure = "CONF_HEALTH_FAILURE"
+	traceEnabled = flag.Bool("trace", false, "whether to include tracing information into responses")
+)
+
+var (
+	timeout     = time.Duration(*timeoutSec) * time.Second
+	serversPool = []string{
+		"server1:8080",
+		"server2:8080",
+		"server3:8080",
+	}
+	serverHealths = []bool{true, true, true}
+)
+
+func scheme() string {
+	if *https {
+		return "https"
+	}
+	return "http"
+}
+
+func health(dst string) bool {
+	ctx, _ := context.WithTimeout(context.Background(), timeout)
+	req, _ := http.NewRequestWithContext(ctx, "GET",
+		fmt.Sprintf("%s://%s/health", scheme(), dst), nil)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return false
+	}
+	if resp.StatusCode != http.StatusOK {
+		return false
+	}
+	return true
+}
+
+func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
+	ctx, _ := context.WithTimeout(r.Context(), timeout)
+	fwdRequest := r.Clone(ctx)
+	fwdRequest.RequestURI = ""
+	fwdRequest.URL.Host = dst
+	fwdRequest.URL.Scheme = scheme()
+	fwdRequest.Host = dst
+
+	resp, err := http.DefaultClient.Do(fwdRequest)
+	if err == nil {
+		for k, values := range resp.Header {
+			for _, value := range values {
+				rw.Header().Add(k, value)
+			}
+		}
+		if *traceEnabled {
+			rw.Header().Set("lb-from", dst)
+		}
+		log.Println("fwd", resp.StatusCode, resp.Request.URL)
+		rw.WriteHeader(resp.StatusCode)
+		defer resp.Body.Close()
+		_, err := io.Copy(rw, resp.Body)
+		if err != nil {
+			log.Printf("Failed to write response: %s", err)
+		}
+		return nil
+	} else {
+		log.Printf("Failed to get response from %s: %s", dst, err)
+		rw.WriteHeader(http.StatusServiceUnavailable)
+		return err
+	}
+}
+
+func hashAddress(addr string) int {
+	ha := strings.Split(strings.Join(strings.Split(addr, "."), ""), ":")[0]
+	hs, err := strconv.Atoi(ha)
+	if err != nil {
+		panic(err)
+	}
+	return hs
+}
+
+func filterHealthy() []string {
+	healthyServersPool := []string{}
+	for i := range serversPool {
+		if serverHealths[i] == true {
+			healthyServersPool = append(healthyServersPool, serversPool[i])
+		}
+	}
+	return healthyServersPool
+}
+
+func balanceRequest(addr string) (string, error) {
+	healthyServersPool := filterHealthy()
+	if len(healthyServersPool) == 0 {
+		return "", errors.New("No servers available")
+	}
+	addrHash := hashAddress(addr)
+	serverIndex := addrHash % len(healthyServersPool)
+	//log.Println(addr, serverIndex, healthyServersPool[serverIndex])
+	return healthyServersPool[serverIndex], nil
+}
+
+func handleRequest(rw http.ResponseWriter, r *http.Request) {
+	server, err := balanceRequest(r.RemoteAddr)
+	if err != nil {
+		rw.WriteHeader(http.StatusServiceUnavailable)
+		_, _ = rw.Write([]byte("FAILURE"))
+		return
+	}
+	forward(server, rw, r)
+}
 
 func main() {
-	h := new(http.ServeMux)
+	flag.Parse()
+	for i, server := range serversPool {
+		server := server
+		go func() {
+			for range time.Tick(10 * time.Second) {
+				serverHealths[i] = health(server)
+				log.Println(server, health(server))
+			}
+		}()
+	}
 
-	h.HandleFunc("/health", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Header().Set("content-type", "text/plain")
-		if failConfig := os.Getenv(confHealthFailure); failConfig == "true" {
-			rw.WriteHeader(http.StatusInternalServerError)
-			_, _ = rw.Write([]byte("FAILURE"))
-		} else {
-			rw.WriteHeader(http.StatusOK)
-			_, _ = rw.Write([]byte("OK"))
-		}
-	})
+	frontend := httptools.CreateServer(*port, http.HandlerFunc(handleRequest))
 
-	report := make(Report)
-
-	h.HandleFunc("/api/v1/some-data", func(rw http.ResponseWriter, r *http.Request) {
-		respDelayString := os.Getenv(confResponseDelaySec)
-		if delaySec, parseErr := strconv.Atoi(respDelayString); parseErr == nil && delaySec > 0 && delaySec < 300 {
-			time.Sleep(time.Duration(delaySec) * time.Second)
-		}
-
-		report.Process(r)
-
-		rw.Header().Set("content-type", "application/json")
-		rw.WriteHeader(http.StatusOK)
-		_ = json.NewEncoder(rw).Encode([]string{
-			"1", "2",
-		})
-	})
-
-	h.Handle("/report", report)
-
-	server := httptools.CreateServer(*port, h)
-	server.Start()
+	log.Println("Starting load balancer...NYA!")
+	log.Printf("Tracing support enabled: %t", *traceEnabled)
+	frontend.Start()
 	signal.WaitForTerminationSignal()
 }
